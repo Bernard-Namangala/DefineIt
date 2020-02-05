48be8dc26070e90db32910e0c234985e2d8bdc6c
unknown <bernardnamangala@gmail.com>
2020-02-05 19:08:26 +0200

    added authentication module

--- DefineitPDFViewer/Authentication/models.py
+++ DefineitPDFViewer/Authentication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# class UserExtension(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     phone = models.IntegerField()
+#     company = models.CharField(max_length=255)
+#     location = models.CharField(max_length=255)
+#     website = models.CharField(max_length=255)
+#     gender = models.CharField(max_length=50, choices=(("male", "male"), ('female', 'female'), ('other', 'other')))
+#
+#     class Meta:
+#         default_related_name = 'user_extension'
+#
+#     def __str__(self):
+#         return self.user.username
